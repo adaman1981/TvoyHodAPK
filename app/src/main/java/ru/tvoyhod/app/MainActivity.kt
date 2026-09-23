f549fafccaceb8069c3bfb4840d312196ce3e284
adaman1981 <adaman1981@googlemail.com>
2026-09-23 16:01:36 +0000

    Diagnostic native launch test

--- app/src/main/java/ru/tvoyhod/app/MainActivity.kt
+++ app/src/main/java/ru/tvoyhod/app/MainActivity.kt
@@ -1,63 +1,23 @@
 package ru.tvoyhod.app
 
-import android.annotation.SuppressLint
 import android.os.Bundle
-import android.view.View
-import android.webkit.WebChromeClient
-import android.webkit.WebResourceRequest
-import android.webkit.WebResourceResponse
-import android.webkit.WebSettings
-import android.webkit.WebView
-import androidx.activity.OnBackPressedCallback
+import android.graphics.Color
+import android.view.Gravity
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.webkit.WebViewAssetLoader
-import androidx.webkit.WebViewClientCompat
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var webView: WebView
-
-    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        webView = WebView(this)
-        setContentView(webView)
 
-        window.statusBarColor = android.graphics.Color.rgb(5,9,11)
-        window.navigationBarColor = android.graphics.Color.rgb(5,9,11)
-
-        webView.settings.apply {
-            javaScriptEnabled = true
-            domStorageEnabled = true
-            databaseEnabled = true
-            mediaPlaybackRequiresUserGesture = false
-            allowFileAccess = false
-            allowContentAccess = false
-            mixedContentMode = WebSettings.MIXED_CONTENT_NEVER_ALLOW
-        }
-        webView.webChromeClient = WebChromeClient()
-
-        val loader = WebViewAssetLoader.Builder()
-            .addPathHandler("/assets/", WebViewAssetLoader.AssetsPathHandler(this))
-            .build()
-
-        webView.webViewClient = object : WebViewClientCompat() {
-            override fun shouldInterceptRequest(view: WebView, request: WebResourceRequest): WebResourceResponse? =
-                loader.shouldInterceptRequest(request.url)
+        val test = TextView(this).apply {
+            text = "ТВОЙ ХОД\n\nANDROID CORE OK"
+            textSize = 26f
+            setTextColor(Color.WHITE)
+            setBackgroundColor(Color.rgb(5, 9, 11))
+            gravity = Gravity.CENTER
         }
 
-        if (savedInstanceState == null)
-            webView.loadUrl("https://appassets.androidplatform.net/assets/index.html")
-        else webView.restoreState(savedInstanceState)
-
-        onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
-            override fun handleOnBackPressed() {
-                if (webView.canGoBack()) webView.goBack() else finish()
-            }
-        })
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        webView.saveState(outState)
-        super.onSaveInstanceState(outState)
+        setContentView(test)
     }
 }
