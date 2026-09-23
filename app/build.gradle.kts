b3f40254493461fb7be02a7638f2788103a72697
adaman1981 <adaman1981@googlemail.com>
2026-09-23 16:17:40 +0000

    Restore Kotlin Android plugin and JVM 17

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,12 @@
-plugins { id("com.android.application") }
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
 android {
     namespace = "ru.tvoyhod.app"
     compileSdk = 35
+
     defaultConfig {
         applicationId = "ru.tvoyhod.app"
         minSdk = 24
@@ -9,9 +14,18 @@ android {
         versionCode = 1
         versionName = "1.0"
     }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
 }
+
 dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation(platform("org.jetbrains.kotlin:kotlin-bom:1.8.22"))
     implementation("androidx.webkit:webkit:1.12.1")
 }
