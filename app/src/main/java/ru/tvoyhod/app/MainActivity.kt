19848e456ef86acb39c88003392140c2c898440b
adaman1981 <adaman1981@googlemail.com>
2026-09-23 16:37:38 +0000

    Load full product on stable WebView core

--- app/src/main/java/ru/tvoyhod/app/MainActivity.kt
+++ app/src/main/java/ru/tvoyhod/app/MainActivity.kt
@@ -2,6 +2,7 @@ package ru.tvoyhod.app
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
@@ -17,65 +18,35 @@ class MainActivity : AppCompatActivity() {
         webView = WebView(this)
         setContentView(webView)
 
-        window.statusBarColor = android.graphics.Color.rgb(5, 9, 11)
-        window.navigationBarColor = android.graphics.Color.rgb(5, 9, 11)
-
         webView.settings.apply {
             javaScriptEnabled = true
             domStorageEnabled = true
+            databaseEnabled = true
             mediaPlaybackRequiresUserGesture = false
+            allowFileAccess = true
+            allowContentAccess = true
         }
 
         webView.webViewClient = WebViewClient()
+        webView.webChromeClient = WebChromeClient()
 
-        val html = """
-            <!doctype html>
-            <html>
-            <head>
-                <meta name="viewport"
-                      content="width=device-width,initial-scale=1">
-                <style>
-                    html,body{
-                        margin:0;
-                        width:100%;
-                        height:100%;
-                        background:#05090b;
-                        color:#fff;
-                        font-family:sans-serif;
-                    }
-                    body{
-                        display:flex;
-                        align-items:center;
-                        justify-content:center;
-                        text-align:center;
-                    }
-                    .ok{
-                        font-size:28px;
-                        line-height:1.6;
-                    }
-                    strong{color:#42dfd1}
-                </style>
-            </head>
-            <body>
-                <div class="ok">
-                    ТВОЙ ХОД<br>
-                    <strong>WEBVIEW CORE OK</strong>
-                </div>
-            </body>
-            </html>
-        """.trimIndent()
-
-        webView.loadDataWithBaseURL(
-            "https://app.local/",
-            html,
-            "text/html",
-            "UTF-8",
-            null
-        )
+        if (savedInstanceState == null) {
+            webView.loadUrl("file:///android_asset/index.html")
+        } else {
+            webView.restoreState(savedInstanceState)
+        }
     }
 
-    override fun onDestroy() {
-        webView.destroy()
-        super.onDestroy()
+    override fun onSaveInstanceState(outState: Bundle) {
+        webView.saveState(outState)
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onBackPressed() {
+        if (::webView.isInitialized && webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
     }
 }
