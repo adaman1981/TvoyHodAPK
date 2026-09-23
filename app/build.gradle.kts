ac6d3a782483cc962564429d777f2b0100f3dfd0
adaman1981 <adaman1981@googlemail.com>
2026-09-23 12:16:47 +0000

    Android V1 project

---
+++ app/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins { id("com.android.application") }
+android {
+    namespace = "ru.tvoyhod.app"
+    compileSdk = 35
+    defaultConfig {
+        applicationId = "ru.tvoyhod.app"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+}
+dependencies {
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.webkit:webkit:1.12.1")
+}
